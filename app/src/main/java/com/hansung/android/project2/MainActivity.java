2f4da98c420b4af162dcb67bb42649cae8d6783a
tae young <xoxodud12@gmail.com>
2017-11-21 14:18:47 +0900

    오영주
    데이터베이스(미완)

--- app/src/main/java/com/hansung/android/project2/MainActivity.java
+++ app/src/main/java/com/hansung/android/project2/MainActivity.java
@@ -2,13 +2,20 @@ package com.hansung.android.project2;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Environment;
+import android.provider.BaseColumns;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -110,3 +117,141 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
+
+public final class UserContract {
+
+
+    public static final String DB_NAME="user.db";
+    public static final int DATABASE_VERSION = 1;
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private UserContract() {}
+
+    /* Inner class that defines the table contents */
+    public static class Users implements BaseColumns {
+        public static final String TABLE_NAME="Users";
+        public static final String KEY_NAME = "Name";
+        public static final String KEY_PHONE = "Phone";
+
+        public static final String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + " (" +
+                _ID + " INTEGER PRIMARY KEY" + COMMA_SEP +
+                KEY_NAME + TEXT_TYPE + COMMA_SEP +
+                KEY_PHONE + TEXT_TYPE +  " )";
+        public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+}
+
+
+public class DBHelper extends SQLiteOpenHelper {
+
+
+    final static String TAG="SQLiteDBTest";
+
+
+    public DBHelper(Context context) {
+        super(context, UserContract.DB_NAME, null, UserContract.DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(UserContract.Users.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+        db.execSQL(UserContract.Users.DELETE_TABLE);
+        onCreate(db);
+    }
+
+    public void insertUserBySQL(String name, String phone) {
+        try {
+            String sql = String.format (
+                    "INSERT INTO %s (%s, %s, %s) VALUES (NULL, '%s', '%s')",
+                    UserContract.Users.TABLE_NAME,
+                    UserContract.Users._ID,
+                    UserContract.Users.KEY_NAME,
+                    UserContract.Users.KEY_PHONE,
+                    name,
+                    phone);
+
+            getWritableDatabase().execSQL(sql);
+        } catch (SQLException e) {
+
+        }
+    }
+
+    public Cursor getAllUsersBySQL() {
+        String sql = "Select * FROM " + UserContract.Users.TABLE_NAME;
+        return getReadableDatabase().rawQuery(sql,null);
+    }
+
+    public void deleteUserBySQL(String _id) {
+        try {
+            String sql = String.format (
+                    "DELETE FROM %s WHERE %s = %s",
+                    UserContract.Users.TABLE_NAME,
+                    UserContract.Users._ID,
+                    _id);
+            getWritableDatabase().execSQL(sql);
+        } catch (SQLException e) {
+
+        }
+    }
+
+    public void updateUserBySQL(String _id, String name, String phone) {
+        try {
+            String sql = String.format (
+                    "UPDATE  %s SET %s = '%s', %s = '%s' WHERE %s = %s",
+                    UserContract.Users.TABLE_NAME,
+                    UserContract.Users.KEY_NAME, name,
+                    UserContract.Users.KEY_PHONE, phone,
+                    UserContract.Users._ID, _id) ;
+            getWritableDatabase().execSQL(sql);
+        } catch (SQLException e) {
+
+        }
+    }
+
+    public long insertUserByMethod(String name, String phone) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(UserContract.Users.KEY_NAME, name);
+        values.put(UserContract.Users.KEY_PHONE,phone);
+
+        return db.insert(UserContract.Users.TABLE_NAME,null,values);
+    }
+
+    public Cursor getAllUsersByMethod() {
+        SQLiteDatabase db = getReadableDatabase();
+        return db.query(UserContract.Users.TABLE_NAME,null,null,null,null,null,null);
+    }
+
+    public long deleteUserByMethod(String _id) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        String whereClause = UserContract.Users._ID +" = ?";
+        String[] whereArgs ={_id};
+        return db.delete(UserContract.Users.TABLE_NAME, whereClause, whereArgs);
+    }
+
+    public long updateUserByMethod(String _id, String name, String phone) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(UserContract.Users.KEY_NAME, name);
+        values.put(UserContract.Users.KEY_PHONE,phone);
+
+        String whereClause = UserContract.Users._ID +" = ?";
+        String[] whereArgs ={_id};
+
+        return db.update(UserContract.Users.TABLE_NAME, values, whereClause, whereArgs);
+    }
+
+}
+
+
