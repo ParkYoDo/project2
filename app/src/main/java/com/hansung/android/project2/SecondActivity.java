e391feffe9d91bd3bd7ca4ee653fe728bdafcc17
HyeSeong Choi <ya9087@naver.com>
2017-11-21 14:44:30 +0900

    박요도
    + 버튼 클릭시 메뉴추가액티비티 전환
    메뉴추가액티비티 레이아웃 (미완)

--- app/src/main/java/com/hansung/android/project2/SecondActivity.java
+++ app/src/main/java/com/hansung/android/project2/SecondActivity.java
@@ -1,17 +1,79 @@
 package com.hansung.android.project2;
 
-import android.os.Build;
-import android.os.Bundle;
-import android.support.annotation.RequiresApi;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
 
-/**
- * Created by B10726 on 2017-11-21.
- */
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class SecondActivity extends AppCompatActivity{
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_activity);
+        setContentView(R.layout.activity_second);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.quick_action1:
+                startActivity(new Intent(this, ThirdActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    String mPhotoFileName;
+    File mPhotoFile;
+
+    final int REQUEST_IMAGE_CAPTURE = 100;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if (mPhotoFileName != null) {
+                mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), mPhotoFileName);
+                Uri uri = Uri.fromFile(mPhotoFile);
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageURI(uri);
+            } else
+                Toast.makeText(getApplicationContext(), "mPhotoFile is null", Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/hansung/android/project2/ThirdActivity.java
@@ -0,0 +1,98 @@
+package com.hansung.android.project2;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by B10726 on 2017-11-21.
+ */
+
+public class ThirdActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+    public void OnClickButton(View v) {
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    private String currentDateFormat(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH_mm_ss");
+        String  currentTimeStamp = dateFormat.format(new Date());
+        return currentTimeStamp;
+    }
+    String mPhotoFileName;
+    File mPhotoFile;
+
+    final int REQUEST_IMAGE_CAPTURE = 100;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            //1. 카메라 앱으로 찍은 이미지를 저장할 파일 객체 생성
+            mPhotoFileName = "IMG"+currentDateFormat()+".jpg";
+            mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), mPhotoFileName);
+
+            if (mPhotoFile !=null) {
+                //2. 생성된 파일 객체에 대한 Uri 객체를 얻기
+                Uri imageUri = FileProvider.getUriForFile(this, "com.hansung.android.project2", mPhotoFile);
+
+                //3. Uri 객체를 Extras를 통해 카메라 앱으로 전달
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            } else
+                Toast.makeText(getApplicationContext(), "file null", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if (mPhotoFileName != null) {
+                mPhotoFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), mPhotoFileName);
+                Uri uri = Uri.fromFile(mPhotoFile);
+                ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                imageView.setImageURI(uri);
+            } else
+                Toast.makeText(getApplicationContext(), "mPhotoFile is null", Toast.LENGTH_SHORT).show();
+        }
+    }
+    final int REQUEST_EXTERNAL_STORAGE_FOR_MULTIMEDIA = 0;
+
+    private void checkDangerousPermissions() {
+        String[] permissions = {
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+
+        int permissionCheck = PackageManager.PERMISSION_GRANTED;
+        for (int i = 0; i < permissions.length; i++) {
+            permissionCheck = ContextCompat.checkSelfPermission(this, permissions[i]);
+            if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                break;
+            }
+        }
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_EXTERNAL_STORAGE_FOR_MULTIMEDIA);
+        }
+    }
+}
